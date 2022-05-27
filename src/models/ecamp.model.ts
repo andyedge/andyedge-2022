59f7a33ee193375229803d31b1a543a43c964725
Juan Pablo Moreno Muñoz <moreno.juan@correounivalle.edu.co>
2022-05-27 18:30:16 -0500

    Ecamp
    The following changes were made:
    
    - Created model for Ecamp page
    - Created model for Standard Card Container
    - Created model for Text Slider
    - Added functions to responseAdapter

---
+++ src/models/ecamp.model.ts
@@ -0,0 +1,30 @@
+import ImageType from "./image.model";
+import ContactContainer from "./contact.model"
+import StandardCardContainer from "./standardCardContainer.model";
+import StandardContainer from "./standardContainer.model";
+
+declare interface Ecamp {
+    heroContainer: StandardContainer
+    cardsContainer: StandardCardContainer[]
+    cardModal1: StandardCardContainer
+    cardModal2: StandardCardContainer
+    cardModal3: StandardCardContainer
+    bigTitle1: string
+    standardContainer1: StandardContainer
+    standardContainer2: StandardContainer
+    standardContainer3: StandardContainer
+    standardContainer4: StandardContainer
+    standardContainer5: StandardContainer
+    standardContainer6: StandardContainer
+    standardContainer7: StandardContainer
+    standardContainer8: StandardContainer
+    standardContainer9: StandardContainer
+    bigTitle2: string
+    pageImages: ImageType[]
+    pageSubtitle: string
+    pageText: string
+    sliderImages: ImageType[]
+    contactContainer: ContactContainer
+}
+
+export default Ecamp
