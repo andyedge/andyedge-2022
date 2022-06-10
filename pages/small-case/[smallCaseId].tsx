00e44b3bf2804f4c91c114a64ef90cacaee91805
Oscar Sandoval <oscar.sandoval@appspring.tech>
2022-06-10 16:40:30 -0500

    Small case studies (WiP)

--- pages/small-case/[smallCaseId].tsx
+++ pages/small-case/[smallCaseId].tsx
@@ -2,12 +2,14 @@ import { GetStaticProps, GetStaticPaths, NextPage } from 'next'
 import { getSmallCaseStudies, searchSmallCaseStudy, getHeader } from '../../src/services/fetch'
 import Header from '../../src/models/header.model'
 import Entry from '../../src/models/entry.model'
-import SmallCaseStudy from '../../src/models/smallCaseStudy'
+import SmallCaseStudy from '../../src/models/smallCaseStudy.model'
 import Layout from '../../src/components/layout/Layout'
 import SmallCaseHero from '../../src/components/smallCaseStudy/hero/Hero'
 import CardsContainer from '../../src/components/cardsContainer/CardsContainer'
 import TextSlider from '../../src/components/textSlider/TextSlider'
 import Banner from '../../src/components/banner/Banner'
+import SmallCaseInfo from '../../src/components/smallCaseStudy/info/Info'
+import PortfolioContainer from '../../src/components/portfolioContainer/PortfolioContainer'
 
 export const getStaticPaths: GetStaticPaths = async () => {
     const smallCaseStudies: Entry = await getSmallCaseStudies()
@@ -38,11 +40,13 @@ declare interface SmallCaseStudyProps {
 
 const SmallCase: NextPage<SmallCaseStudyProps> = ({ pageContent, header }: SmallCaseStudyProps) => (
     <Layout header={header}>
-        <div className='container-1440'>
-            {pageContent.hero && <SmallCaseHero data={pageContent.hero} /> }
-            {pageContent.cards.length && <CardsContainer contents={pageContent.cards} scrollToRef={null} smallSpaccing={true}/> }
-            {pageContent.textSlider && <TextSlider data={pageContent.textSlider} /> }
-            {pageContent.banner && <Banner src={pageContent.banner} /> }
+        <div className='container-fluid'>
+            <SmallCaseHero data={pageContent.hero} />
+            <CardsContainer contents={pageContent.cards} scrollToRef={null} smallSpaccing={true}/>
+            <Banner src={pageContent.banner} />
+            <SmallCaseInfo data={pageContent.caseStudyInfo} />
+            <TextSlider contents={[pageContent.textSlider]} />
+            <PortfolioContainer />
         </div>
     </Layout>
 )
---
+++ src/components/smallCaseStudy/info/Info.tsx
@@ -0,0 +1,61 @@
+import { StudyCaseInfo } from '../../../models/smallCaseStudy.model'
+import CustomImage from '../../image/Image'
+import styles from './Info.module.sass'
+import Button from '../../button/Button'
+
+const categories = [{
+    color: 'purple',
+    name: 'UX DESIGN'
+},
+{
+    color: 'red',
+    name: 'LOGO DESIGN'
+},
+{
+    color: 'yellow',
+    name: 'GRAPHIC DESIGN'
+}]
+
+const SmallCaseHero = ({ data } : { data : StudyCaseInfo }) => (
+    <div className={'container'}>
+        <div className={styles.container}>
+            <div className={styles.left_row}>
+                <div className={styles.labels}>
+                    {categories.map((category, index) => (
+                        <h6 key={index} className={`status-${category.color}`}>
+                            {category.name}
+                        </h6>
+                    ))}
+                </div>
+                <h2 className='secondary-title'>{data.title}</h2>
+                <p className='info-text'>{data.description}</p>
+                <div className={styles.carousel}>
+                    {data.carousel.length && <CustomImage src={data.carousel[0]}/>}
+                </div>
+            </div>
+            <div className={styles.right_row}>
+                <div>
+                    <div>
+                        {data.properties.length && data.properties.map((property, index) => (
+                            <div className={styles.item} key={index}>
+                                <div className={styles.parameter}>{property.label}</div>
+                                <div>{property.value}</div>
+                            </div>
+                        ))}
+                    </div>
+                    <div className={styles.text}>
+                        <p>{data.extendedText}</p>
+                    </div>
+                    <div className={styles.list}>
+                        {data.bulletPoints.map((bullet, index) => (
+                            <p className={styles.box} key={index}>{bullet.title}</p>
+                        ))}
+                    </div>
+                </div>
+                <Button link={data.primaryCta?.url} text={data.primaryCta?.text} size='small'/>
+            </div>
+        </div>
+    </div>
+)
+
+export default SmallCaseHero
