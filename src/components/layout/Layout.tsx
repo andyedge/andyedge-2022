d9401a9b43146649e29874e143c7b3be74a8a938
Oscar Sandoval <oscar.sandoval@appspring.tech>
2022-07-13 10:47:57 -0500

    Added favicon and page title

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,9 +2,13 @@ import{ FC } from 'react'
 import Header from '../header/Header'
 import Footer from '../footer/Footer'
 import LayoutModel from '../../models/generic/layout.model'
+import Head from 'next/head'
 
 const Layout: FC<LayoutModel> = (props: LayoutModel) => (
   <>
+    <Head>
+      <title>Andyedge</title>
+    </Head>
     <Header data={props.header}/>
     {props.children}
     <Footer data={props.footer}/>
