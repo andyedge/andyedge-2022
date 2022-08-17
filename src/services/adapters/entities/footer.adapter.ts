ca9da4425a9552f627d1938f93dadacd074f2a8d
Oscar Sandoval <oscar.sandoval@appspring.tech>
2022-08-17 10:44:44 -0500

    Added Social media icons in footer

--- src/services/adapters/entities/footer.adapter.ts
+++ src/services/adapters/entities/footer.adapter.ts
@@ -3,6 +3,17 @@ import Footer from '../../../models/entities/footer.model'
 import { adaptImage } from '../generic/image.adapter'
 import { adaptLink } from '../generic/link.adapter'
 
+const adaptSocialLinks = (socials: Item[]) => {
+    return socials.map((social) => {
+        const { fields } = social
+        return {
+            icon: fields.preTitle,
+            url: fields.title,
+            size: fields.size
+        }
+    })
+}
+
 export const adaptFooterData = (data: Entry): Footer => {
     const [header] = data.items
     const { fields } = header
@@ -14,6 +25,7 @@ export const adaptFooterData = (data: Entry): Footer => {
         getInTouchText: fields.getInTouchText,
         getInTouchContent: fields.getInTouchContent,
         button: adaptLink(fields.button),
-        copyrightText: fields.copyrightText
+        copyrightText: fields.copyrightText,
+        socialLinks: adaptSocialLinks(fields.socialLinks)
     }
 }
