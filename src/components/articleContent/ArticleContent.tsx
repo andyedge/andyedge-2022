12ecf32ed382353527275733b500b2f354911b9c
Juan Pablo Moreno Muñoz <moreno.juan@correounivalle.edu.co>
2022-11-08 14:26:21 -0500

    Article pages
    The following changes were made:
    
    - Modified Rich Text component to admit entry references in the rich text fields
    - Removed unused sections of the Article page
    - Made style modifications to the Article pages

--- src/components/articleContent/ArticleContent.tsx
+++ src/components/articleContent/ArticleContent.tsx
@@ -12,68 +12,8 @@ declare interface ArticleProps {
 
 const ArticleContent: FC<ArticleProps> = ({ contents, scrollToRef }: ArticleProps) => (
     <div className={'container'}>
-        <div>
-            <CustomImage
-                src={contents.standardContainer1.logoC}
-            />
-        </div>
-        <section className={styles.first_section} ref={scrollToRef}>
-            <div>
-                <div className={styles.info}>
-                    <h3>{contents.standardContainer1.title}</h3>
-                    <p>{contents.standardContainer1.subtitle}</p>
-                </div>
-                <div className={styles.inline_images}>
-                    <CustomImage
-                        src={contents.standardContainer1.logoC}
-                    />
-                </div>
-            </div>
-            <div>
-                <div className={styles.info}>
-                    <h3>{contents.standardContainer2.title}</h3>
-                    <p>{contents.standardContainer2.subtitle}</p>
-                </div>
-                <div className={styles.inline_images}>
-                    <CustomImage
-                        src={contents.standardContainer2.logoC}
-                    />
-                </div>
-            </div>
-        </section>
-        <section className={styles.carousel}>
-            {contents.mediaCarousel.length ? (
-                <div>
-                    <CustomImage src={{image: contents.mediaCarousel[0]}} />
-                </div>
-            )
-                : null}
-        </section>
         <section className={styles.article_body}>
             <RichText richText={contents.articleText} />
-        </section>
-        <section className={styles.images_section}>
-            {
-                contents.pageImage1.image.url !== '' ?
-                    <div>
-                        <CustomImage
-                            src={contents.pageImage1}
-                        />
-                    </div>
-                    :
-                    null
-            }
-            {
-                contents.pageImage3.image.url !== '' ?
-                    <div>
-                        <CustomImage
-                            src={contents.pageImage3}
-                            props={{ customAttr: { objectFit: 'contain' } }}
-                        />
-                    </div>
-                    :
-                    null
-            }
         </section>
     </div>
 )
