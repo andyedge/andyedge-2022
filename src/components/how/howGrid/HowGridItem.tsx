68f27cbda0ad4bc2b7e10068d1ecf6145041fd11
Juan Pablo Moreno Muñoz <moreno.juan@correounivalle.edu.co>
2022-11-21 18:01:25 -0500

    How page
    The following changes were made:
    
    - Added image to HowGridItem
    - Added styles for image in HowGridItem

--- src/components/how/howGrid/HowGridItem.tsx
+++ src/components/how/howGrid/HowGridItem.tsx
@@ -1,39 +1,44 @@
 import styles from './HowGrid.module.sass'
-import HowItem from '../../../models/generic/howItem.model'
 import CustomImage from '../../image/Image'
+import HowItem from '../../../models/generic/howItem.model'
 
 declare interface HowGridItemProps {
-    item: HowItem
+  item: HowItem
 }
 
-const HowGridItem = ({ item }: HowGridItemProps) => (
+const HowGridItem = ({ item }: HowGridItemProps) => {
+  const clickFunctionGridItem = () => {
+  }
+  
+  return (
     <div className={styles.item}>
-        <div className={styles.item_image}>
-            {
-                item.category ?
-                    <div className={styles.category}>
-                        <h6 className='status-light-black'>{item.category.name}</h6>
-                    </div>
-                    :
-                    null
-            }
-        </div>
-        <div className={styles.item_info}>
-            {
-                item.platform ?
-                    <div className={styles.item_platform_icon}>
-                        <CustomImage src={{ image: item.platform.icon }} />
-                    </div>
-                    :
-                    null
-            }
-            <div>
-                <h5>{item.title}</h5>
-                <h6>{item.description}</h6>
-                <h6>{item.date}</h6>
+      <div className={styles.item_image} onClick={clickFunctionGridItem}>
+        <CustomImage src={{ image: item.itemImage }} />
+        {
+          item.category ?
+            <div className={styles.category}>
+              <h6 className='status-light-black'>{item.category.name}</h6>
             </div>
+            :
+            null
+        }
+      </div>
+      <div className={styles.item_info}>
+        {
+          item.platform ?
+            <div className={styles.item_platform_icon}>
+              <CustomImage src={{ image: item.platform.icon }} />
+            </div>
+            :
+            null
+        }
+        <div>
+          <h5>{item.title}</h5>
+          <h6>{item.description}</h6>
         </div>
+      </div>
     </div>
-)
+  )
+}
 
 export default HowGridItem
